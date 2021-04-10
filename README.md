482da0ad0a80c0b6ec2b2a94832337762a4b6b07
Myles O'Connor <myles.oconnor@gmail.com>
2021-04-10 09:52:15 -0700

    API for products and random user

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Testing out Django with Python 3.
 
 ### Commands
 
-Run containers `backend` (Django) and `db` (MySQL) using Docker:
+Run containers `backend` (Django) and `db` (MySQL):
 
 ```
 docker-compose up
@@ -16,8 +16,16 @@ Enter `backend` container:
 docker-compose exec backend sh
 ```
 
+### To Do
+
+- [ ] Replace `requirements.txt` with `pipenv`, `Pipfile`, `Pipfile.lock`
+- [ ] Handle env vars with `django-environ`
+- [ ] Add API documentation with `django-rest-swagger`
+
 ### Notes
 
 - Started project `django-admin startproject admin`
 - Run local server `python3 manage.py runserver`
 - Create new Django app inside Docker container `python manage.py startapp products`
+- Make Django database migrations `python manage.py makemigrations`
+- Run migrations `python manage.py migrate`
--- products/views.py
+++ products/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import random
 
-# Create your views here.
+from .models import Product, User
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # GET /api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):  # POST /api/products
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):  # GET /api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):  # PUT /api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):  # DELETE /api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(APIView):
+    def get(self, _):  # GET /api/user
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id
+        })
